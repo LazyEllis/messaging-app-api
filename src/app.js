913b42b90e7d7dab47396388675ce8a46a7bbfa3
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2026-03-21 22:06:19 +0100

    Add real-time messaging functionality

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import { Server } from "socket.io";
+import http from "node:http";
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
@@ -10,6 +12,13 @@ import messageRouter from "./routes/messageRouter.js";
 import indexRouter from "./routes/indexRouter.js";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+  },
+});
 
 app.use(express.json());
 app.use(cors({ origin: process.env.FRONTEND_URL }));
@@ -21,6 +30,28 @@ app.use("/channels/:channelId/recipients", recipientRouter);
 app.use("/channels/:channelId/messages", messageRouter);
 app.use("/", indexRouter);
 
+io.on("connection", (socket) => {
+  socket.on("join-channel", (channelId) => {
+    socket.join(`channel-${channelId}`);
+  });
+
+  socket.on("send-message", (channelId, message) => {
+    socket.to(`channel-${channelId}`).emit("sent-message", message);
+  });
+
+  socket.on("edit-message", (channelId, message) => {
+    socket.to(`channel-${channelId}`).emit("edited-message", message);
+  });
+
+  socket.on("delete-message", (channelId, messageId) => {
+    socket.to(`channel-${channelId}`).emit("deleted-message", messageId);
+  });
+
+  socket.on("leave-channel", (channelId) => {
+    socket.leave(`channel-${channelId}`);
+  });
+});
+
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   console.error(err);
@@ -28,6 +59,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
